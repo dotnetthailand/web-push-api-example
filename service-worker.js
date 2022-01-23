f308e45f14dff0a7a0bb185b457a6b1c2e39a903
aaronamm <aaronamm@users.noreply.github.com>
2022-01-23 11:44:23 +0000

    deploy: 4febe6d69770d004564452b13a720ab7e7447d5a

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,11 @@
-self.addEventListener('push', () => {
-  self.registration.showNotification('Hello world', {
-    body: 'Buzz! Buzz!',
-    vibrate: [300, 100, 400], // Vibrate 300ms, pause 100ms, then vibrate 400ms
-    icon: './favicon.ico',
-    tag: 'vibration-sample'
-  });
+self.addEventListener('push', (event) => {
+  const data = e.data.json();
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: 'Buzz! Buzz!',
+      vibrate: [300, 100, 400], // Vibrate 300ms, pause 100ms, then vibrate 400ms
+      icon: './favicon.ico',
+      tag: 'vibration-sample'
+    })
+  );
 });
