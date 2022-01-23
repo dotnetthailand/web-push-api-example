424fb4c5eebd6be05e836948f9241e7829e53e61
aaronamm <aaronamm@users.noreply.github.com>
2022-01-23 11:11:05 +0000

    deploy: 00cf9a6d50248943bb30b6033aadadad2b0c83c5

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,8 @@
 self.addEventListener('push', () => {
   self.registration.showNotification('Hello world!');
+  self.registration.showNotification('Vibration Sample', {
+    body: 'Buzz! Buzz!',
+    vibrate: [200, 100, 200, 100, 200, 100, 200],
+    tag: 'vibration-sample'
+  });
 });
