1d42c99d95ffc0dab560cfdbfe2faac9c2256379
aaronamm <aaronamm@users.noreply.github.com>
2022-01-23 11:16:34 +0000

    deploy: e3afd5533d412ca474db30c3e5fcdd9c087cdf49

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('push', () => {
   self.registration.showNotification('Hello world', {
     body: 'Buzz! Buzz!',
-    vibrate: [200, 100, 200, 100, 200, 100, 200],
+    vibrate: [300, 100, 400], // Vibrate 300ms, pause 100ms, then vibrate 400ms
     icon: './favicon.ico',
     tag: 'vibration-sample'
   });
